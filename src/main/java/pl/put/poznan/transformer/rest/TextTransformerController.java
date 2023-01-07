731dc8f14710443cc6234b88d78a03a9de1a3024
zcienka <zuzannacienka@gmail.com>
2023-01-07 01:10:53 +0100

    Fix transformation mistake

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -5,41 +5,46 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class TextTransformerController {
     @RequestMapping(value = "/{transformation:.+}", method = RequestMethod.POST)
     public ResponseEntity<Response> post(@PathVariable String transformation, @RequestBody Request request) {
-        Text newText;
+        Text newText = new PlainText(request.text);
+        String[] transformations = transformation.split(" ");
 
-        switch (transformation) {
-            case "capitalize":
-                newText = new Capitalize(new ToLower(new PlainText(request.text)));
-                break;
-            case "uppercase":
-                newText = new ToUpper(new PlainText(request.text));
-                break;
-            case "lowercase":
-                newText = new ToLower(new PlainText(request.text));
-                break;
-            case "expand-abbreviation":
-                newText = new ExpandAbbreviations(new PlainText(request.text));
-                break;
-            case "latex":
-                newText = new LatexFormat(new PlainText(request.text));
-                break;
-            case "eliminate-duplicates":
-                newText = new DuplicatesElimination(new PlainText(request.text));
-                break;
-            case "inverse":
-                newText = new Inverse(new PlainText(request.text));
-                break;
-            case "words-to-abbreviations":
-                newText = new WordsToAbbreviations(new PlainText(request.text));
-                break;
-            default:
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        for (String singleTransformation : transformations) {
+            switch (singleTransformation) {
+                case "capitalize":
+                    newText = new Capitalize(new ToLower(newText));
+                    break;
+                case "uppercase":
+                    newText = new ToUpper(newText);
+                    break;
+                case "lowercase":
+                    newText = new ToLower(newText);
+                    break;
+                case "expand-abbreviation":
+                    newText = new ExpandAbbreviations(newText);
+                    break;
+                case "latex":
+                    newText = new LatexFormat(newText);
+                    break;
+                case "eliminate-duplicates":
+                    newText = new DuplicatesElimination(newText);
+                    break;
+                case "inverse":
+                    newText = new Inverse(newText);
+                    break;
+                case "words-to-abbreviations":
+                    newText = new WordsToAbbreviations(newText);
+                    break;
+                default:
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
         }
         return ResponseEntity.ok(new Response(newText.get()));
     }
